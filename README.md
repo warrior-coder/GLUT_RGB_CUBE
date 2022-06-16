e753062df34db661a5a26a7f859633d98fd98470
warrior-coder <gudkou2003@gmail.com>
2022-06-16 13:42:33 +0300

    More screenshots.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Standart cube rotation animation in graphic window on C with OpenGL Utility Toolkit (GLUT).
 
 ![Screenshot1](Screenshots/Screenshot1.gif)
+
+![Screenshot2](Screenshots/Screenshot2.png)
+
+![Screenshot3](Screenshots/Screenshot3.png)
--- rgb_cube/rgb_cube.c
+++ rgb_cube/rgb_cube.c
@@ -3,6 +3,13 @@
 #define NUM_VERTICES 8
 #define NUM_FACES 6
 
+
+// вращение цветового куба RGB
+// поскольку куб является выпуклым многогранником и единственный элемент на сцене, его можно отрисовывать, используя только отсечение задней грани (нет необходимости в буфере глубины)
+
+
+// куб имеет противоположные углы в точках {0,0,0} и {1,1,1}, которым соответствуют черные и белые вершины
+// Ox — красный градиент, Oy — зеленый градиент, Oz — синий градиент
 GLint vertices[NUM_VERTICES][3] = {
 	{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
 	{1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}
@@ -20,39 +27,41 @@ GLfloat vertexColors[NUM_VERTICES][3] = {
 
 GLfloat rotateAngle = 0.0f;
 
+// анимация достигается путем очистки окна и отрисовки нового положения куба
 void DisplayFunc()
 {
+    // очистка окна
     glClearColor(0.5f, 0.5f, 0.5f, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-     
     glPushMatrix();
+
     glLoadIdentity();
 
+    // вращение куба
     glScalef(0.75f, 0.75f, 0.75f);
-    glTranslatef(0, -0.84f,  0);
+    glTranslatef(0, -0.84f, -2.0f);
     glRotatef(54.73561032f, -1, 0, 1);
 
 	glRotatef(rotateAngle, 1, 1, 1);
     rotateAngle += 1.5f;
 
+    // отрисовка граней
 	glBegin(GL_QUADS);
     for (int i = 0; i < NUM_FACES; ++i)
     {
         for (int j = 0; j < 4; ++j)
         {
-            glColor3fv((GLfloat*)(vertexColors + faceIndexes[i][j]));
-            glVertex3iv((GLint*)(vertices + faceIndexes[i][j]));
+            glColor3fv((GLfloat*)(vertexColors + faceIndexes[i][j])); // задает текущий цвет из указанного массива GLfloat[3]
+            glVertex3iv((GLint*)(vertices + faceIndexes[i][j])); // задает координаты текущей вершины из указанного массива GLint[3]
         }
     }
     glEnd();
 
     glPopMatrix();
 
-    glFlush();
-    glutSwapBuffers();
+    glFlush(); // очищает все буферы и заставляет все ранее выданные команды выполниться
+    glutSwapBuffers(); // окно имеет двойную буферизацию, поэтому меняем местами буферы, чтобы сделать рисунок видимым и получить анимацию без мерцания
 }
 
 void TimerFunc(int value)
@@ -61,15 +70,36 @@ void TimerFunc(int value)
     glutTimerFunc(10, TimerFunc, value);
 }
 
+void ReshapeFunc(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (GLdouble)width / (GLdouble)height, 0.5, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+// единственное, что нужно сделать в начале это включить отсечение задней грани
+void Initialize()
+{
+    glEnable(GL_DEPTH_TEST);
+}
+
+// точка входа GLUT-приложения
 int main(int argc, char** argv)
 {
+    // создание окна
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(512, 512);
-    glutCreateWindow("RGB Cube");
-    
+    glutCreateWindow("GLUT RGB Cube");
+
+    // привязка функций-обработчиков к событиям
+	glutReshapeFunc(ReshapeFunc);
     glutTimerFunc(10, TimerFunc, 0);
     glutDisplayFunc(DisplayFunc);
+
+    Initialize();
 
 	glutMainLoop();
 }
